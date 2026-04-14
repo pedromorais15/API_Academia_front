2b05f9f39784b58b68c0ff7f482a661e21402443
pedromorais15 <pedro.morais.senai@gmail.com>
2026-04-14 15:04:06 -0300

    2.4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://api-academia-five.vercel.app/'; 
+const API_BASE_URL = 'https://api-academia-five.vercel.app'; 
 
 const loginSection = document.getElementById('loginSection');
 const adminSection = document.getElementById('adminSection');
